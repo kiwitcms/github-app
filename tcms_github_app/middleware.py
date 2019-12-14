f12d73171557d7fcd2e02e280848f6e289f2a8a8
Mr. Senko <atodorov@mrsenko.com>
2019-12-14 23:50:18 +0200

    CheckGitHubAppMiddleware and basic admin
    this middleware will warn & redirect users to configure their
    integration with GitHub. And we don't take NO for an answer.
    
    - admin allows users to edit only their own apps, otherwise 403

---
+++ tcms_github_app/middleware.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2019 Alexander Todorov <atodorov@MrSenko.com>
+
+# Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
+# pylint: disable=too-few-public-methods
+
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+
+from social_django.models import UserSocialAuth
+
+from tcms_github_app.models import AppInstallation
+
+
+class CheckGitHubAppMiddleware:
+    """
+        Warns the user and redirects them to configure their GitHub App
+        installation so it knows on which tenant to create new objects in DB.
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if not request.user.is_authenticated or request.method != "GET":
+            return self.get_response(request)
+
+        app_inst = None
+        social_user = UserSocialAuth.objects.filter(user=request.user).first()
+        if social_user:
+            app_inst = AppInstallation.objects.filter(
+                sender=social_user.uid,
+                tenant_pk=None
+            ).first()
+
+        if app_inst:
+            admin_path = reverse('admin:tcms_github_app_appinstallation_change',
+                                 args=[app_inst.pk])
+            if request.path != admin_path:
+                messages.add_message(
+                    request,
+                    messages.WARNING,
+                    _('Unconfigured GitHub App %d') % app_inst.installation,
+                    fail_silently=True)
+                return HttpResponseRedirect(admin_path)
+
+        return self.get_response(request)
---
+++ tcms_github_app/tests/test_middleware.py
@@ -0,0 +1,67 @@
+# Copyright (c) 2019 Alexander Todorov <atodorov@MrSenko.com>
+
+# Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
+# pylint: disable=too-many-ancestors
+
+from django.test import modify_settings
+from django.urls import reverse
+
+from tcms_tenants.tests import LoggedInTestCase
+
+from tcms_github_app.tests import AppInstallationFactory
+from tcms_github_app.tests import UserSocialAuthFactory
+
+
+class CheckGitHubAppMiddlewareTestCase(LoggedInTestCase):
+    @modify_settings(MIDDLEWARE={
+        'append': 'tcms_github_app.middleware.CheckGitHubAppMiddleware',
+    })
+    def test_nothing_for_anonymous_user(self):
+        self.client.logout()
+
+        response = self.client.get('/', follow=True)
+
+        self.assertContains(response, 'Welcome Guest')
+        self.assertNotContains(response, 'Unconfigured GitHub App')
+
+    @modify_settings(MIDDLEWARE={
+        'append': 'tcms_github_app.middleware.CheckGitHubAppMiddleware',
+    })
+    def test_redirects_when_unconfigured_installation(self):
+        # simulate unconfigured installation
+        social_user = UserSocialAuthFactory(user=self.tester)
+        app_inst = AppInstallationFactory(sender=int(social_user.uid))
+        expected_url = reverse('admin:tcms_github_app_appinstallation_change',
+                               args=[app_inst.pk])
+
+        response = self.client.get('/', follow=True)
+
+        self.assertRedirects(response, expected_url)
+        self.assertContains(response, 'Unconfigured GitHub App %d' % app_inst.installation)
+
+    @modify_settings(MIDDLEWARE={
+        'append': 'tcms_github_app.middleware.CheckGitHubAppMiddleware',
+    })
+    def test_nothing_when_user_doesnt_have_social_auth(self):
+        # simulate unconfigured installation but the current user
+        # created his account via email/password, not GitHub so we
+        # can't match the GitHub uid !
+        AppInstallationFactory(sender=999999)
+
+        response = self.client.get('/')
+
+        self.assertContains(response, 'Kiwi TCMS - Dashboard')
+        self.assertNotContains(response, 'Unconfigured GitHub App')
+
+    @modify_settings(MIDDLEWARE={
+        'append': 'tcms_github_app.middleware.CheckGitHubAppMiddleware',
+    })
+    def test_nothing_when_unconfigured_installation_not_owned_by_user(self):
+        # simulate unconfigured installation by another user
+        social_user = UserSocialAuthFactory()
+        AppInstallationFactory(sender=int(social_user.uid))
+
+        response = self.client.get('/')
+
+        self.assertContains(response, 'Kiwi TCMS - Dashboard')
+        self.assertNotContains(response, 'Unconfigured GitHub App')
