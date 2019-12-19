1c3fa2428d1c02b4b39d552c70eb3ade4dc4aa5f
Mr. Senko <atodorov@mrsenko.com>
2019-12-19 22:33:15 +0200

    tests: Do not include tcms_github_app.urls
    the plugin loading code in tcms/urls.py should do this
    automatically so we don't need to do it ourselves

--- test_project/urls.py
+++ test_project/urls.py
@@ -6,12 +6,9 @@ from django.conf.urls import include, url
 
 from tcms.urls import urlpatterns
 from tcms_tenants import urls as tenants_urls
-from tcms_github_app import urls as githubapp_urls
 
 
 urlpatterns += [
-    url(r'^github/app/', include(githubapp_urls)),
     url(r'^tenants/', include(tenants_urls, namespace='tenants')),
-
     url('', include('social_django.urls', namespace='social')),
 ]
