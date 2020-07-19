5f204f2c62635d06c55982aee1aafb5d5f99494b
Mr. Senko <atodorov@mrsenko.com>
2020-07-19 23:26:53 +0300

    Refactor utility functions to work with Repository objects
    because some payloads do not have all of the necessary information
    we talk directly to GitHub to give us a full Repository object and
    use its attributes as we see fit.
    
    - add expected settings vars in test_project/settings.py for testing

--- tcms_github_app/utils.py
+++ tcms_github_app/utils.py
@@ -3,8 +3,10 @@
 # Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
 #
 
+from django.conf import settings
 from django.core.cache import cache
 
+import github
 from django_tenants.utils import tenant_context
 from social_django.models import UserSocialAuth
 
@@ -15,6 +17,33 @@ from tcms.testcases.models import BugSystem
 
 from tcms_tenants.models import Tenant
 from tcms_github_app.models import AppInstallation
+
+
+def find_token_from_app_inst(gh_app, installation):
+    """
+        Find an installation access token for this app:
+        https://docs.github.com/en/rest/reference/apps#create-an-installation-access-token-for-an-app
+
+        and cache it for 50 mins!
+    """
+    cache_key = "token-for-%d" % installation.installation
+
+    token = cache.get(cache_key)
+    if not token:
+        token = gh_app.get_access_token(installation.installation)
+        token = token.token
+        # token expires after 1 hr so cache it for 50 mins
+        cache.set(cache_key, token, 3000)
+
+    return token
+
+
+def github_rpc_from_inst(installation):
+    gh_app = github.GithubIntegration(settings.KIWI_GITHUB_APP_ID,
+                                      settings.KIWI_GITHUB_APP_PRIVATE_KEY)
+
+    token = find_token_from_app_inst(gh_app, installation)
+    return github.Github(token)
 
 
 def find_user_from_sender(sender_id):
@@ -32,6 +61,8 @@ def find_user_from_sender(sender_id):
 
 def find_tenant(data):
     """
+        return (tenant, app_inst)
+
         Return a Tenant for this installation or None
         if installation doesn't exist/isn't configured yet.
     """
@@ -42,15 +73,20 @@ def find_tenant(data):
     if not app_inst:
         return None
 
-    return Tenant.objects.filter(pk=app_inst.tenant_pk).first()
+    tenant = Tenant.objects.filter(pk=app_inst.tenant_pk).first()
+
+    return tenant, app_inst
 
 
-def _product_from_repo(repo_data):
+def _product_from_repo(repo_object):
+    """
+        repo_object is a github.Repository.Repository object
+    """
     # skip forks
-    if repo_data.get('fork', False):
+    if repo_object.fork:
         return None
 
-    name = repo_data['full_name']
+    name = repo_object.full_name
 
     product = Product.objects.filter(name=name).first()
 
@@ -62,7 +98,7 @@ def _product_from_repo(repo_data):
     if product:
         return product
 
-    description = repo_data.get('description', '')
+    description = repo_object.description
     if not description:
         description = 'GitHub repository'
 
@@ -74,30 +110,36 @@ def _product_from_repo(repo_data):
     )
 
 
-def _bugtracker_from_repo(repo_data):
+def _bugtracker_from_repo(repo_object):
+    """
+        repo_object is a github.Repository.Repository object
+    """
     # skip forks
-    if repo_data.get('fork', False):
+    if repo_object.fork:
         return None
 
-    name = repo_data['full_name']
+    name = repo_object.full_name
     bug_system, _created = BugSystem.objects.get_or_create(
         name='GitHub Issues for %s' % name,
         tracker_type='tcms_github_app.issues.Integration',
-        base_url=repo_data['html_url'],
+        base_url=repo_object.html_url,
     )
     return bug_system
 
 
 def create_product_from_repository(data):
-    tenant = find_tenant(data)
+    tenant, installation = find_tenant(data)
 
     # can't handle requests from unconfigured installation
     if not tenant:
         return
 
     with tenant_context(tenant):
-        _product_from_repo(data.payload['repository'])
-        _bugtracker_from_repo(data.payload['repository'])
+        rpc = github_rpc_from_inst(installation)
+        repo_object = rpc.get_repo(data.payload['repository']['full_name'])
+
+        _product_from_repo(repo_object)
+        _bugtracker_from_repo(repo_object)
 
 
 def create_installation(data):
@@ -123,7 +165,7 @@ def create_installation(data):
         tenant = user.tenant_set.first()
         tenant_pk = tenant.pk
 
-    AppInstallation.objects.create(
+    installation = AppInstallation.objects.create(
         installation=data.payload['installation']['id'],
         sender=data.sender,
         tenant_pk=tenant_pk,
@@ -131,20 +173,25 @@ def create_installation(data):
 
     if tenant and tenant_pk:
         with tenant_context(tenant):
+            rpc = github_rpc_from_inst(installation)
             for repository in data.payload['repositories']:
-                _product_from_repo(repository)
-                _bugtracker_from_repo(repository)
+                repo_object = rpc.get_repo(repository['full_name'])
+                _product_from_repo(repo_object)
+                _bugtracker_from_repo(repo_object)
 
 
 def create_version_from_tag(data):
-    tenant = find_tenant(data)
+    tenant, _installation = find_tenant(data)
 
     # can't handle requests from unconfigured installation
     if not tenant:
         return
 
     with tenant_context(tenant):
-        product = _product_from_repo(data.payload['repository'])
+        # in case we've missed the repo creation hooks create a new Product & BugSystem
+        create_product_from_repository(data)
+        product = Product.objects.filter(name=data.payload['repository']['full_name']).first()
+
         if not product:
             return
 
@@ -152,22 +199,3 @@ def create_version_from_tag(data):
             value=data.payload['ref'],
             product=product
         )
-
-
-def find_token_from_app_inst(gh_app, installation):
-    """
-        Find an installation access token for this app:
-        https://docs.github.com/en/rest/reference/apps#create-an-installation-access-token-for-an-app
-
-        and cache it for 50 mins!
-    """
-    cache_key = "token-for-%d" % installation.installation
-
-    token = cache.get(cache_key)
-    if not token:
-        token = gh_app.get_access_token(installation.installation)
-        token = token.token
-        # token expires after 1 hr so cache it for 50 mins
-        cache.set(cache_key, token, 3000)
-
-    return token
