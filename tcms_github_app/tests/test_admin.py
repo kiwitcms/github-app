c032654dec01c0aa64839dca6e1ae6656fe17252
Mr. Senko <atodorov@mrsenko.com>
2020-08-04 16:35:13 +0300

    Add tests for AppInstallationAdmin.save_form(), Refs #20

--- tcms_github_app/tests/test_admin.py
+++ tcms_github_app/tests/test_admin.py
@@ -3,6 +3,8 @@
 # Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
 # pylint: disable=too-many-ancestors
 
+import unittest.mock
+
 from django.urls import reverse
 from django.http import HttpResponseForbidden
 
@@ -138,8 +140,10 @@ class AppInstallationAdminTestCase(LoggedInTestCase):
                 reverse('admin:tcms_github_app_appinstallation_change', args=[self.app_inst.pk]))
             self.assertIsInstance(response, HttpResponseForbidden)
 
-    def test_save_by_owner_updates_installation(self):
-        self.assertIsNone(self.app_inst_tester.tenant_pk)
+    @unittest.mock.patch('tcms_github_app.utils.resync')
+    def test_save_by_owner_updates_installation(self, utils_resync):
+        self.app_inst_tester.tenant_pk = None
+        self.app_inst_tester.save()
 
         response = self.client.post(
             reverse('admin:tcms_github_app_appinstallation_change', args=[self.app_inst_tester.pk]),
@@ -156,6 +160,64 @@ class AppInstallationAdminTestCase(LoggedInTestCase):
 
         self.app_inst_tester.refresh_from_db()
         self.assertEqual(self.app_inst_tester.tenant_pk, self.tenant.pk)
+
+        # resync was triggered b/c we've updated tenant_pk
+        utils_resync.assert_called_once()
+
+    @unittest.mock.patch('tcms_github_app.utils.resync')
+    def test_change_tenant_pk_to_empty_doesnt_resync(self, utils_resync):
+        self.app_inst_tester.tenant_pk = self.tenant.pk
+        self.app_inst_tester.save()
+
+        # turning off middleware b/c it will redirect to the AppInst edit page
+        with self.modify_settings(MIDDLEWARE={
+                'remove': 'tcms_github_app.middleware.CheckGitHubAppMiddleware',
+        }):
+            response = self.client.post(
+                reverse('admin:tcms_github_app_appinstallation_change',
+                        args=[self.app_inst_tester.pk]),
+                {
+                    'tenant_pk': '',
+                },
+                follow=True,
+            )
+
+            self.assertRedirects(response, '/')
+            self.assertContains(response, 'The app installation')
+            self.assertContains(response, self.app_inst_tester)
+            self.assertContains(response, 'was changed successfully.')
+
+            self.app_inst_tester.refresh_from_db()
+            self.assertIsNone(self.app_inst_tester.tenant_pk)
+
+            # resync was not triggered b/c tenant_pk was set to None
+            utils_resync.assert_not_called()
+
+    @unittest.mock.patch('tcms_github_app.utils.resync')
+    def test_save_not_changing_anything_doesnt_resync(self, utils_resync):
+        self.app_inst_tester.tenant_pk = self.tenant.pk
+        self.app_inst_tester.save()
+
+        # this is the same as if user just pressed the Save button without making
+        # any changes
+        response = self.client.post(
+            reverse('admin:tcms_github_app_appinstallation_change', args=[self.app_inst_tester.pk]),
+            {
+                'tenant_pk': self.tenant.pk,
+            },
+            follow=True,
+        )
+
+        self.assertRedirects(response, '/')
+        self.assertContains(response, 'The app installation')
+        self.assertContains(response, self.app_inst_tester)
+        self.assertContains(response, 'was changed successfully.')
+
+        self.app_inst_tester.refresh_from_db()
+        self.assertEqual(self.app_inst_tester.tenant_pk, self.tenant.pk)
+
+        # resync was not triggered b/c tenant_pk wasn't changed
+        utils_resync.assert_not_called()
 
 
 class WebhookPayloadAdminTestCase(LoggedInTestCase):
