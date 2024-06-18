5b9815ebb7a4cb629167460dba9e9f52f3727fbd
Alexander Todorov <atodorov@otb.bg>
2024-06-18 10:09:06 +0300

    Filter social auths only for 'github-app' instead of using .first()
    Fixes KIWI-TCMS-P5, https://kiwitcms.sentry.io/issues/5501934208/?project=277775
    
    To reproduce the original bug:
    
    1) Login via Google OAuth - uid is recorded as the email address
    2) Login via GitHhub App integration - uid is an integer
    3) Try editing the GitHub App via PLUGINS -> GitHub Integration ->
       Settings
    
    The first social_auth record will be picked up and will lead to a
    ValueError b/c the query expects an integer, not a string!
    
    With this commit the code will search for record where
    provider='github-app' because this is what it needs!

--- tcms_github_app/middleware.py
+++ tcms_github_app/middleware.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019-2022 Alexander Todorov <atodorov@otb.bg>
+# Copyright (c) 2019-2024 Alexander Todorov <atodorov@otb.bg>
 #
 # Licensed under GNU Affero General Public License v3 or later (AGPLv3+)
 # https://www.gnu.org/licenses/agpl-3.0.html
@@ -30,8 +30,8 @@ class CheckGitHubAppMiddleware:
             return self.get_response(request)
 
         app_inst = None
-        social_user = request.user.social_auth.first()
-        if social_user and social_user.uid.isdigit():
+        social_user = request.user.social_auth.filter(provider='github-app').first()
+        if social_user:
             app_inst = AppInstallation.objects.filter(
                 sender=social_user.uid,
                 tenant_pk=None
