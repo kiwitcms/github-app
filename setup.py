0827a1ffaf1bfa568cfe79d8a594e7b0d7d61e01
Alexander Todorov <atodorov@otb.bg>
2024-06-18 10:23:28 +0300

    Changelog for v2.0.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def get_install_requires(path):
 
 setup(
     name='kiwitcms-github-app',
-    version='2.0.0',
+    version='2.0.1',
     description='GitHub App integration for Kiwi TCMS',
     long_description=get_long_description(),
     author='Kiwi TCMS',
